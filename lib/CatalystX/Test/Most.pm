f6a48fa4d35201f1aeb9de6ba910309048de24e2
Ashley Pond V <ashley@cpan.org>
2012-08-12 23:27:13 -0700

    Add repo to Makefile.PL, bump version in anticipation, add Changes.

--- lib/CatalystX/Test/Most.pm
+++ lib/CatalystX/Test/Most.pm
@@ -4,7 +4,7 @@ use HTTP::Request::Common ( qw{ GET POST DELETE PUT } );
 use Test::More;
 use Test::Fatal;
 our $AUTHORITY = "cpan:ASHLEY";
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 our @EXPORT = ( qw{ GET POST DELETE PUT },
                 qw{ request ctx_request action_redirect },
                 qw{ exception },
