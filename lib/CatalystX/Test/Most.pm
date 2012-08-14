d04b3a572f967a71162eac2bf160db741dc35e26
Ashley Pond V <ashley@cpan.org>
2012-08-13 18:23:12 -0700

    Missing dep.

--- lib/CatalystX/Test/Most.pm
+++ lib/CatalystX/Test/Most.pm
@@ -4,7 +4,7 @@ use HTTP::Request::Common ( qw{ GET POST DELETE PUT } );
 use Test::More;
 use Test::Fatal;
 our $AUTHORITY = "cpan:ASHLEY";
-our $VERSION = "0.02";
+our $VERSION = "0.03";
 our @EXPORT = ( qw{ GET POST DELETE PUT },
                 qw{ request ctx_request action_redirect },
                 qw{ exception },
